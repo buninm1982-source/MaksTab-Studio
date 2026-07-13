4cb984b059755a60d203d4c25d3372c67ad8bc82
buninm1982-source <buninm1982@gmail.com>
2026-07-13 16:19:39 +0500

    MaksTab Studio v0.2.1 photo fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='makstab-v02';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./assets/maks-photo.jpg'];
+const CACHE='makstab-v021';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./maks-photo.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
