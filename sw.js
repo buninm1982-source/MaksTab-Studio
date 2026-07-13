68cb41c12417a5e2b71c7794431c7627a6c251b8
buninm1982-source <buninm1982@gmail.com>
2026-07-13 16:13:14 +0500

    MaksTab Studio v0.2

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-self.addEventListener('fetch',()=>{});
+const CACHE='makstab-v02';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./assets/maks-photo.jpg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
